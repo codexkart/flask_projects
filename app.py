d5d8de1c7e5ab2c6da7acf4a6e2368d94088e438
Sahil Mehta <sahilbmehta290303@gmail.com>
2023-06-24 12:06:39 +0530

    final commit with ready website

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,jsonify
-from database import load_jobs_from_db,load_job_from_db
+from flask import Flask,render_template,jsonify,request
+from database import load_jobs_from_db,load_job_from_db,add_application_to_db
 
 app = Flask(__name__)
 
@@ -19,6 +19,13 @@ def show_job(id):
     if not job:
         return "NOT FOUND", 404
     return render_template('jobpage.html',job = job)
+
+@app.route("/job/<id>/apply",methods = ['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id,data)
+    return render_template('appllication_submission.html',application = data,job = job)
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
